fafd4de7923108c837cf9530188da730a1e7325b
dissna <123608388+dissna@users.noreply.github.com>
2023-07-07 00:36:29 +0300

    add main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,16 @@ from faker import Faker
 
 faker = Faker('uk_UA')
 
-
+def generate_random_contact():
+    # Generates and prints a random contact
+    full_name = faker.name()
+    phone_number = faker.phone_number()
+    full_address = faker.address()
+    while not phone_number.startswith('+380'):
+        phone_number = faker.phone_number()
+    print(f'Full name: {full_name}\n'
+          f'Phone number: {phone_number}\n'
+          f'Full address: {full_address}')
 
 
 def main():
